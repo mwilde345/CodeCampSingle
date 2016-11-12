de4cf70b9460e3412afbe6e6b610d79c2d57c3ff
Josh Hess <joshhess13@gmail.com>
2016-11-12 02:22:20 -0700

    Xbox Input

---
+++ network-full-dive/Assets/Scripts/GameState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour {
+
+    static bool pause = true;
+    static float timer, reset = 0.18f;
+    
+    void Update() {
+        if (timer >= reset) {
+            if (Input.GetButton( "Start" )) {
+                timer = 0f;
+                pause = !pause;
+                print( pause );
+            }
+        }
+        else timer += Time.deltaTime;
+    }
+	
+    public static bool isPaused() {
+        return pause;
+    }
+}
