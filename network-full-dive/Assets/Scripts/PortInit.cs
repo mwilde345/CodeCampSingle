e104c33a95f5bc04824178ed69f0d43dfd9f03ae
Josh Hess <joshhess13@gmail.com>
2016-11-11 19:38:06 -0700

    Fixed Port Spawn Distribution

--- network-full-dive/Assets/Scripts/PortInit.cs
+++ network-full-dive/Assets/Scripts/PortInit.cs
@@ -14,15 +14,28 @@ public class PortInit : MonoBehaviour {
         float phi = 0;
         while(k <= n) {
             if (k == n) phi = 0;
-            center.transform.position = new Vector3(0, 0, 0);
-            center.transform.rotation = Quaternion.identity;
-            center.transform.rotation *= Quaternion.Euler( ( ( 360 * phi ) / ( 2 * Mathf.PI ) ), ( ( 360 * theta ) / ( 2 * Mathf.PI ) ), 0 );
-            center.transform.Translate(Vector3.forward * radius, Space.Self);
-            portPoints[(int)k - 1] = new Vector3(center.transform.position.x, center.transform.position.y, center.transform.position.z);
+
+            print(k + ", " + h + ", " + theta + ", " + phi);
+            
+            float x = radius * Mathf.Sin( phi ) * Mathf.Cos( theta );
+            float y = radius * Mathf.Sin( phi ) * Mathf.Sin( theta );
+            float z = radius * Mathf.Cos( phi );
+
+
+            // center.transform.position = new Vector3(0, 0, 0);
+            // enter.transform.rotation = Quaternion.identity;
+            // center.transform.rotation *= Quaternion.Euler( ( ( 360 * phi ) / ( 2 * Mathf.PI ) ), ( ( 360 * theta ) / ( 2 * Mathf.PI ) ), 0 );
+            // center.transform.Translate(Vector3.forward * radius, Space.Self);
+
+            //portPoints[(int)k - 1] = new Vector3(center.transform.position.x, center.transform.position.y, center.transform.position.z);
+
+            portPoints[(int) k - 1] = new Vector3(x, y, z);
+
+
             k++;
             h = -1 + ( (2 * ( k - 1 )) / ( n - 1 ) );
             theta = Mathf.Acos( h );
-            phi = (float)(phi + (3.6 / Mathf.Sqrt(n)) * (1 / Mathf.Sqrt(1 - (h*h)))) % (2 * Mathf.PI);
+            phi += (float)((3.6 / Mathf.Sqrt(n)) * (1 / Mathf.Sqrt(1 - (h*h)))) % (2 * Mathf.PI);
         }
 
         center.transform.position = new Vector3( 0, 0, 0 );
